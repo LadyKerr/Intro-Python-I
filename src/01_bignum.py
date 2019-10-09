bd0bbea806db41839597fcfbc4a31559be03d38b
LadyKerr <kadiekerr@gmail.com>
2019-10-09 13:41:36 -0400

    bignum

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,11 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
+#JS = Math.pow(2, 65536) => outputs Infinity!
 
 # YOUR CODE HERE
+x = 2
+y = 65536
+
+# Using ** means to the power of
+print(x ** y)
+
