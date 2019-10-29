821a4ed43837ecccab55e75b1e0fd62ec1a93f29
LadyKerr <kadiekerr@gmail.com>
2019-10-28 23:21:51 -0400

    dictionaries

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,9 +36,23 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+waypoints.append({
+    "lat": 40,
+    "lon": 120,
+    "name": "the last place"
+})
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
 
+#since this is a list, we can access it with indexes. The one that needs to be changed is at the first position.
+waypoints[0]["name"] = "not a real place"
+waypoints[0]["lon"] = -130
+
+
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for values in waypoints:
+    print(values["name"], values["lat"], values["lon"])
